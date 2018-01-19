63cf89ed6787980813b7825fc5c34f1f6cd52025
Maverick Tesini <mavericktesini@gmail.com>
2018-01-19 19:52:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ function fareDoubler(double) {
 function fareTripler(triple) {return triple * 3 };
 
 function selectDifferentDrivers(arrayOfDrivers, drivers) {
-  return drivers;
+  return drivers(arrayOfDrivers);
 }
