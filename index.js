63a7af61f6370184a18f3432770bb2616b00ba28
Maverick Tesini <mavericktesini@gmail.com>
2018-01-19 19:39:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const returnFirstTwoDrivers = function(array) {return array.slice(0,2) };
 const returnLastTwoDrivers = function(array) {return array.slice(array.length-2) };
 
 //array containing first two functions
-let array1 = [returnFirstTwoDrivers, returnLastTwoDrivers];
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
