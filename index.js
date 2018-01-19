15fd668196f47a4a1f24788ac01a0b82d7b09374
Maverick Tesini <mavericktesini@gmail.com>
2018-01-19 19:21:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Code your solution in this file!
 
-function (returnFirstTwoDrivers(array) {
-  const a1 = array.slice(0,2);
-  return a1;
-} )
+function returnFirstTwoDrivers(array) {
+  return array.slice(0,2);
+}
