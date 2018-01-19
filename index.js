cc21b5f3bf9809a57c817a1504903528d65f4cd5
Maverick Tesini <mavericktesini@gmail.com>
2018-01-19 19:20:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 
-const a2 = function returnFirstTwoDrivers(array) {
+function (returnFirstTwoDrivers(array) {
   const a1 = array.slice(0,2);
   return a1;
-}
+} ) {}
