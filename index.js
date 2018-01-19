676997a684452d3eff164c78bf563da03a99b218
Maverick Tesini <mavericktesini@gmail.com>
2018-01-19 19:35:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Code your solution in this file!
 
 const returnFirstTwoDrivers = function(array) {return array.slice(0,2) };
+// or the alternate syntax: const returnFirstTwoDrivers = array => array.slice(0,2);   ...cleaner but somewhat foreign and harder to remember
